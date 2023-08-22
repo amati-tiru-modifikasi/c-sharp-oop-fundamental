427da73c55854b6363100a4863dd3ce4d3ba2203
mDestaFadilah <desta.08b@gmail.com>
2023-08-22 14:30:45 +0700

    part #11 akses product class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CSharpFundamentals;
+
+Product entity = new();
+
+entity.ProductId = 1;
+entity.Name = "Test";
+
+Console.WriteLine(entity.ProductId);
+Console.WriteLine(entity.Name);
