09feafecf14c4834042f25c1ea3497a1d4af780f
mDestaFadilah <desta.08b@gmail.com>
2023-08-23 03:50:22 +0700

    part #13 full properti, private public

--- Product.cs
+++ Product.cs
@@ -3,5 +3,25 @@
     public class Product {
         public int ProductId { get; set; }
         public string Name { get; set; }
+
+        private decimal _StandarCost;
+
+        public decimal StandarCost
+        {
+            get { return _StandarCost; }
+            set { _StandarCost = value; }
+        }
+
+        private decimal _ListPrice;
+
+        public decimal ListPrice
+        {
+            get { return _ListPrice; }
+            set { _ListPrice = value; }
+        }
+
     }
+
+ 
 }
+
--- Program.cs
+++ Program.cs
@@ -3,9 +3,14 @@
 Product entity = new()
 {
     ProductId = 1,
-    Name = "Test"
+    Name = "Test",
+    StandarCost = 55,
+    ListPrice = 20
 };
 
 
 Console.WriteLine(entity.ProductId);
 Console.WriteLine(entity.Name);
+Console.WriteLine('-');
+Console.WriteLine(entity.StandarCost);
+Console.WriteLine(entity.ListPrice);
