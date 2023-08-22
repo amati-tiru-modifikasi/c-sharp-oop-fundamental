30f0f0496e200618ce790480f3ae1ea6587ac27a
mDestaFadilah <desta.08b@gmail.com>
2023-08-23 04:12:05 +0700

    self-paced lab

---
+++ Customer.cs
@@ -0,0 +1,33 @@
+﻿
+namespace CSharpFundamentals
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        private string _CompanyName;    
+
+        public string CompanyName
+        {
+            get { return _CompanyName; }
+            set { _CompanyName = value; }
+        }
+
+        private string _EmailAddress;
+
+        public string EmailAddress
+        {
+            get { return _EmailAddress; }
+            set { _EmailAddress = value; }
+        }
+
+        private decimal _CreditLimit;
+        public decimal CreditLimit
+        {
+            get { return _CreditLimit; }
+            set { _CreditLimit = value; }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using CSharpFundamentals;
 
+/*
 Product entity = new()
 {
     ProductId = 1,
@@ -8,10 +9,35 @@ Product entity = new()
     ListPrice = 6.99M
 };
 
-
 Console.WriteLine(entity.ProductId);
 Console.WriteLine(entity.Name);
 Console.WriteLine('-');
 Console.WriteLine(entity.StandarCost);
 Console.WriteLine(entity.ListPrice);
 Console.WriteLine(entity.Profit.ToString("c"));
+*/
+
+/* SINTAK #2 */
+Customer entity = new()
+{
+    CustomerId = 1,
+    FirstName = "Desta",
+    LastName = "Hacker",
+    CompanyName = "PT Angin Ribu",
+    EmailAddress = "mdestafadilah@gmail.com"
+};
+
+/* SINTAK #1
+entity.CustomerId = 1;
+entity.FirstName = "Desta";
+entity.LastName = "Hacker";
+*/
+
+Console.Write(entity.CustomerId);
+Console.Write(" - ");
+Console.Write(entity.FirstName);
+Console.Write(" ");
+Console.Write(entity.LastName);
+Console.Write(entity.CompanyName);
+Console.Write(entity.EmailAddress);
+
